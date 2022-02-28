4dd219409ac14208a1762d570f29cf71473674e4
Naskichan <gitlab@naski.org>
2022-02-28 16:51:45 +0100

    basic data mocking

---
+++ backend/app/src/data.mock.ts
@@ -0,0 +1,57 @@
+export const pizzas = [
+  {
+    name: 'Margherita',
+    price: 5,
+    ingredients: ['tomato', 'mozzarella'],
+  },
+  {
+    name: 'Bufala',
+    price: 6,
+    ingredients: ['tomato', 'mozarella di bufala'],
+  },
+  {
+    name: 'Romana',
+    price: 5,
+    ingredients: ['tomato', 'mozzarella', 'anchovies', 'oregano', 'oil'],
+  },
+  {
+    name: 'Diavola',
+    price: 7.5,
+    ingredients: ['tomato', 'mozzarella', 'spicy salami'],
+  },
+  {
+    name: 'Pizza Bianca',
+    price: 5,
+    ingredients: ['mozzarella', 'oregano'],
+  },
+];
+export const ingredients = [];
+export const orders = [
+  {
+    orderItems: [],
+  },
+];
+
+/*
+{
+    orderItems: [
+      {
+        pizza: {
+          ingredients: ['tomato', 'mozzarella'],
+          name: 'Magherita',
+          price: 5,
+        },
+        count: 2,
+      },
+      {
+        pizza: {
+          ingredients: [{ name: 'mozzarella' }, { name: 'oregano' }],
+          name: 'Pizza Bianca',
+          price: 5,
+        },
+        count: 2,
+      },
+    ],
+  },
+];
+*/
