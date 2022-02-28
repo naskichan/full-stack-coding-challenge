140ea1a47821d0855d8d1dde0f27416a50ffba91
Naskichan <gitlab@naski.org>
2022-02-28 17:17:19 +0100

    building add function

--- backend/app/src/app.service.ts
+++ backend/app/src/app.service.ts
@@ -1,10 +1,16 @@
 import { orders } from './data.mock';
 import { Order } from './interfaces/order.interface';
 import { Injectable } from '@nestjs/common';
+import { OrderItemDTO } from './interfaces/orderItem.dto';
 
 @Injectable()
 export class AppService {
   ordersData = orders;
+  addOrder(orderItems: OrderItemDTO[]) {
+    return this.ordersData[
+      this.ordersData.push({ orderItems: orderItems }) - 1
+    ];
+  }
   getOrders(): Order[] {
     return this.ordersData;
   }
