49620e536fd8f91a9fddaf497a2e99d55653c793
Naskichan <gitlab@naski.org>
2022-02-28 16:03:40 +0100

    simple orders endpoint

--- backend/app/src/order.controller.ts
+++ backend/app/src/order.controller.ts
@@ -7,19 +7,23 @@ import {
     Put,
     Delete,
   } from '@nestjs/common';
-import { IngredientService } from './ingredient.service';
+import { OrderService } from './order.service';
 import { PizzaService } from './pizza.service';
-import { Pizza as PizzaModel, Ingredient as IngredientModel} from '@prisma/client'
+import { Pizza as PizzaModel, Order as OrderModel} from '@prisma/client'
 
 @Controller()
 export class OrderController {
     constructor(
-        private readonly ingredientService: IngredientService,
+        private readonly orderService: OrderService,
         private readonly pizzaService: PizzaService,
     ) {}
 
     @Get('/pizza')
     async getPizzas(): Promise<PizzaModel[]> {
         return this.pizzaService.getAll();
+    }
+    @Get('/orders')
+    async getOrders(): Promise<OrderModel[]> {
+        return this.orderService.getAll()
     }
 }
