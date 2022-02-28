bdfd269653cc47db6ca3e7ef937096eb42624a97
Naskichan <gitlab@naski.org>
2022-02-28 21:10:16 +0100

    ordering

--- frontend/app/src/App.svelte
+++ frontend/app/src/App.svelte
@@ -1,27 +1,58 @@
 <script>
+	import Pizza from './components/Pizza.svelte'
 	async function fetchData() {
 		const pizzas = await fetch('http://localhost:3000/pizzas')
 		.then((res) => res.json())
 		return pizzas
 	}
 	let promise = fetchData()
-	function handleClick() {
-		promise = fetchData()
+	let orderedPizzas = []
+
+	function handleClick(ordering, payload) {
+		console.log('was clicked')
+		if(ordering) {
+			orderedPizzas = [...orderedPizzas, payload.pizza]
+		} else {
+			console.log(payload)
+			orderedPizzas = orderedPizzas.filter((value) => {value.name !== payload.pizza.name})
+		}
 	}
 
 </script>
 
-<main>
-	<h1>Hungry? Want to eat one of our pizzas? Take a look!</h1>
-	{#await promise}
-		<p>Fetching server data</p>
-	{:then pizzas} 
-	{#each pizzas as pizza}
-		{pizza.name}
-	{/each}
-	{/await}
-	<button on:click={handleClick}>Hello</button>
-</main>
+	<center>
+		<h1>Hungry? Want to eat one of our pizzas? Take a look!</h1>
+		{#await promise}
+			<p>Fetching server data</p>
+		{:then pizzas} 
+			<div class='pizzas'>
+				<h2>Your order </h2>
+				{#each orderedPizzas as pizza, count}
+					<Pizza props={pizza} on:click={() => handleClick(false, {pizza})}/>
+				{/each}
+			</div>
+			<div class='pizzas'>
+				<h2>Explore our menu</h2>
+				{#each pizzas as pizza}
+					<Pizza props={pizza} on:click={() => handleClick(true, {pizza})}/>
+				{/each}
+			</div>
+		{/await}
+	</center>
 
 <style>
+	.pizzas {
+		display: flex;
+		width: 60%;
+		margin: 5rem;
+		padding: 2rem;
+		border-radius: 4px;
+		background-color: #36363F;
+		box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+	}
+	:global(body) {
+		background-color: #444451;
+		color: white;
+		font-family: 'Open Sans', sans-serif;
+	}
 </style>
